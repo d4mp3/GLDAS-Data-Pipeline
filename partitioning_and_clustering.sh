53466c5338c58d2ea83802743cd092e3c4c2c41a
d4mp3 <damian.pszczola@gmail.com>
2024-04-19 01:32:05 +0200

    add partitioning and clustering

---
+++ partitioning_and_clustering.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Create external table
+bq mk \
+--external_table_definition=gs://damdezoomcamp-terra-bucket/gldas_prcp.parquet \
+--table damdezoomcamp:gldas_dataset.external_gldas_prcp
+
+# Create partitioned and clustered table
+bq mk \
+--time_partitioning_type=DAY \
+--time_partitioning_field=date \
+--clustering_fields=month \
+--schema './schema.json' \
+--table damdezoomcamp:gldas_dataset.prcp_daily_partitioned_clustered
